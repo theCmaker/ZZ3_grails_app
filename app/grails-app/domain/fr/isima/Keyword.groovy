6070eed9da30b71692362a8c89e3712bdcc40660
Benjamin Barbesange <Benjamin Barbesange>
2016-12-14 17:29:45 +0100

    Relation experiment on keyword and question

--- app/grails-app/domain/fr/isima/Keyword.groovy
+++ app/grails-app/domain/fr/isima/Keyword.groovy
@@ -5,8 +5,8 @@ class Keyword {
     String value
 
     // Many to many with a question
-    // static hasMany = [ questions : Question ]
-    // static belongsTo = [ question : Question ]
+    static hasMany = [ questions : Question ]
+    static belongsTo = Question
 
     static constraints = {
     }
--- app/grails-app/domain/fr/isima/Question.groovy
+++ app/grails-app/domain/fr/isima/Question.groovy
@@ -2,10 +2,12 @@ package fr.isima
 
 class Question {
 
+    // TODO:validator for tag in list of all tags or none
+
     String title
 
     // Many to many keywords
-    // static hasMany = [ tags: Keyword[] ]
+    static hasMany = [ tags: Keyword[] ]
 
     // One to one Topic
     // static belongsTo = [ topic: Topic ]
