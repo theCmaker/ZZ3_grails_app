ae3d5dec27407b4d55c1a7e2e5cd347630bf31e3
BBS007 <b.barbesange@outlook.com>
2017-02-15 14:25:31 +0100

    Removed module.id in components

--- angular/src/app/answer/answer.component.ts
+++ angular/src/app/answer/answer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'answer',
     templateUrl: 'answer.component.html'
 })
--- angular/src/app/feature/feature.component.ts
+++ angular/src/app/feature/feature.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'feature',
     templateUrl: 'feature.component.html'
 })
