1e5a865bd9dac53b1291ca3b7a118e0a809a5369
BBS007 <b.barbesange@outlook.com>
2017-03-05 16:17:08 +0100

    Added A LOT of services and modified components.
    Still a loooot todo ...

--- angular/src/app/question/question.component.ts
+++ angular/src/app/question/question.component.ts
@@ -4,34 +4,42 @@ import { Location } from '@angular/common';
 
 import 'rxjs/add/operator/switchMap';
 
-import {QuestionService} from './question.service';
+import { QuestionService } from './question.service';
 import { Question } from './question';
 
+import { VoteComponent } from '../vote/vote.component'
+import { AnswerComponent } from '../answer/answer.component';
+
 @Component({
-  selector: 'question',
-  templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css'],
+    selector: 'question',
+    templateUrl: './question.component.html',
+    styleUrls: ['./question.component.css'],
+    entryComponents: [VoteComponent, AnswerComponent]
 })
-  
+
 export class QuestionComponent implements OnInit {
 
 
-  @Input() id: number;
+    @Input() id: number;
 
-  question: Question;
+    question: Question;
 
-  constructor(
-    private questionService: QuestionService,
-    private route: ActivatedRoute,
-    private location : Location
-  ) {
-    
-  }
+    constructor(
+        private questionService: QuestionService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {
+
+    }
 
 
-  ngOnInit(): void {
-    this.route.params
-      .switchMap((params: Params) => this.questionService.getQuestionById(+params['id'])).subscribe(res => { this.question = res; });
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.questionService.getQuestionById(+params['id'])).subscribe(res => { this.question = res; });
+    }
+
+    createAnswer(): void {
+        console.log("create answer");
     }
 
 }
--- angular/src/app/user/user.component.ts
+++ angular/src/app/user/user.component.ts
@@ -1,11 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { User } from './user';
+import { UserService } from './user.service';
+
 
 @Component({
     selector: 'user',
-    templateUrl: 'user.component.html'
+    templateUrl: 'user.component.html',
+    styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    // Id of the user    
+    @Input() id: number;
+
+    user: User;
+
+    constructor(
+        private userService: UserService,
+        private route: ActivatedRoute,
+        private location : Location
+    ) { }
+
+    ngOnInit() {
+        this.route.params
+      .switchMap((params: Params) => this.userService.getUserById(+params['id'])).subscribe(res => { this.user = res; });
+    }
 }
